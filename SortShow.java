95a467078a9433fc960dc5e5e89307c3c269f095
Mahmoud <mahmoodgharbawi@hotmail.com>
2023-02-17 03:33:07 -0600

    updated authors

--- SortShow.java
+++ SortShow.java
@@ -17,7 +17,7 @@ public class SortShow extends JPanel {
 	// An array to hold the lines_lengths to be sorted
 	public int[] lines_lengths;
 	//The amount of lines needed
-	public final int total_number_of_lines = 160;
+	public final int total_number_of_lines = 256;
 	// An array to holds the scrambled lines_lengths
 	public int[] scramble_lines;
 	//A temp Array that is used later for sorts
